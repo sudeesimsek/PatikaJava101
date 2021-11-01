c7bd1d20637f1ad5ce16c1b6d6706940312e72fb
sudeesimsek <sudeeesimsek@gmail.com>
2021-11-01 21:36:49 +0300

    Odev9

---
+++ src/Modul2/mantiksalOperatorler.java
@@ -0,0 +1,22 @@
+package Modul2;
+
+public class mantiksalOperatorler {
+    public static void main(String[] args) {
+        int a=10,b=20,c=30;
+        boolean kosul1=(a<b);
+        boolean kosul2=(a>c);
+        boolean sonuc=kosul1&&kosul2;
+        System.out.println(!sonuc);
+
+        //soru işareti
+
+        int d=10,e=20,f;
+        // karşılaştırma operatörleri ile yapılmalı
+        c=(d==e)? 5:6;
+        System.out.println(c);
+
+
+
+
+    }
+}
